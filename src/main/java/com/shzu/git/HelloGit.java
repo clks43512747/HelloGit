9e233bbf183fc8ff3b7e73776cbed81102fc0a12
xialei <xialei2019@126.com>
2020-07-07 21:38:01 +0800

    初始化项目

---
+++ src/main/java/com/shzu/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.shzu.git;
+
+/**
+ * @author:xialei
+ * @date: 2020/7/7 21:34
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
